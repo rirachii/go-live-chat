8e400685241acf20722deba5bf60b9b66982097b
korey <koreyliu1221@gmail.com>
2024-02-20 03:33:15 -0500

    htmx installation into client dir and servs test html/css files

--- server.go
+++ server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"encoding/json"
 )
 
 const (
@@ -13,13 +14,47 @@ const (
 	Port = "8080"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "LIVE CHAT SERVERRR!")
+// func handleHome(w http.ResponseWriter, r *http.Request) {
+
+	
+// 	fmt.Fprintf(w, "LIVE CHAT SERVERRR!")
+
+// }
+
+func random_msgs(w http.ResponseWriter, r *http.Request) {
+
+
+	msg := "welcome to the unknown"
+
+	msgData := map[string]string {
+		"msg": msg,
+		"msg2": "I am a random message",
+	}
+
+	jsonData, err := json.Marshal(msgData)
+    if err != nil {
+        http.Error(w, "Failed to marshal JSON", http.StatusInternalServerError)
+        return
+    }
+
+
+
+	w.Header().Set("Content-Type", "application/json")
+    w.Write(jsonData)
 }
 
+
+
 func main() {
-	http.HandleFunc("/", home)
-	err := http.ListenAndServe(Host+":"+Port, nil)
+	address := fmt.Sprintf("%s:%s", Host, Port)
+
+	http.Handle("/", http.FileServer(http.Dir("client")))
+
+	http.HandleFunc("/random-msgs", random_msgs)
+
+	fmt.Println("Listening on:", fmt.Sprintf("http://%s",address))
+
+	err := http.ListenAndServe(address, nil)
 	if err != nil {
 		log.Fatal("Error Starting the HTTP Server : ", err)
 		return
