5003143813457317150858e71858aaad4836417f
korey <koreyliu1221@gmail.com>
2024-03-07 03:14:31 -0500

    comments in user_repository.go + added getting user by token and ID

--- service/user_repository.go
+++ service/user_repository.go
@@ -11,14 +11,18 @@ import (
 type UserRepository interface {
 	CreateUser(ctx context.Context, user *model.User) (*model.User, error)
 	GetUserByEmail(ctx context.Context, email string) (*model.User, error)
+	GetUserByID(ctx context.Context, email string) (*model.User, error)
+	GetUserByToken(ctx context.Context, email string) (*model.User, error)
 }
 
 // flexibility can pass in transaction instead of db object
+// uses sql.DB methods/interfaces
+
 type DBTX interface {
 	ExecContext(ctx context.Context, query string, args ...interface{}) (sql.Result, error)
-	PrepareContext(context.Context, string) (*sql.Stmt, error)
-	QueryContext(context.Context, string, ...interface{}) (*sql.Rows, error)
-	QueryRowContext(context.Context, string, ...interface{}) *sql.Row
+	PrepareContext(ctx context.Context, query string) (*sql.Stmt, error)
+	QueryContext(ctx context.Context, query string, args ...interface{}) (*sql.Rows, error)
+	QueryRowContext(ctx context.Context, query string, args ...interface{}) *sql.Row
 }
 
 type repository struct {
@@ -45,11 +49,48 @@ func (r *repository) GetUserByEmail(ctx context.Context, email string) (*model.U
 	u := model.User{}
 
 	query := "SELECT id, email, username, password FROM users WHERE email = $1"
-	err := r.db.QueryRowContext(ctx, query, email).Scan(&u.ID, &u.Email, &u.Username, &u.Password)
+	err := r.db.QueryRowContext(ctx, query, email).
+		Scan(&u.ID, &u.Email, &u.Username, &u.Password)
+
 	if err != nil {
 		return &model.User{}, nil
 	}
 	fmt.Print(u.Username)
 
 	return &u, nil
+}
+
+func (r *repository) GetUserByID(ctx context.Context, userID string) (*model.User, error) {
+
+	u := model.User{}
+
+	query := "SELECT id, email, username, password FROM users WHERE id = $1"
+	err := r.db.QueryRowContext(ctx, query, userID).
+		Scan(&u.ID, &u.Email, &u.Username, &u.Password)
+
+	if err != nil {
+		return &model.User{}, nil
+	}
+	fmt.Print(u.Username)
+
+	return &u, nil
+
+}
+
+// TODO i think we should add a token field to user data or a new table of token -> userID
+func (r *repository) GetUserByToken(ctx context.Context, token string) (*model.User, error) {
+
+	u := model.User{}
+
+	query := "SELECT id, email, username, password FROM users WHERE email = $1"
+	err := r.db.QueryRowContext(ctx, query, token).
+		Scan(&u.ID, &u.Email, &u.Username, &u.Password)
+
+	if err != nil {
+		return &model.User{}, nil
+	}
+	fmt.Print(u.Username)
+
+	return &u, nil
+
 }
