78a682d16808589ffdc87dd5e0e59906b6a83f20
Korey Liu <78379350+korliu@users.noreply.github.com>
2024-04-09 18:39:54 -0400

    Update 20240403061354_user_display_names.up.sql
    missing comma

--- service/db/migrations/20240403061354_user_display_names.up.sql
+++ service/db/migrations/20240403061354_user_display_names.up.sql
@@ -1,5 +1,5 @@
 CREATE TABLE user_display_names(
     user_id BIGINT NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users
+    FOREIGN KEY (user_id) REFERENCES users,
     display_name VARCHAR(20)
 );
